fc3d0d3e6676b856d2c32dc8078986d7c1918b4d
artemdudkin <sheeva@list.ru>
2016-11-02 14:48:03 +0300

    add config

--- parse_filter.js
+++ parse_filter.js
@@ -1,16 +1,14 @@
 const { operator } = require('./filter_operators');
-const operator_keys = Object.keys(operator);
-operator_keys.sort((a,b)=>{return b.length - a.length}); // if "=" comes before "*=" then "=" will win while it is really "*=", not "="
 
 //returns {left:<left_side>, right:<right_side>, delimiter:<delimiter>}
 //for instance, "a\\=b=c" becomes {left:'a=b', right:'c', delimiter:'='}
-const parse_filter = (str) => {
+const parse_filter = (str, opt) => {
 	if (typeof str === 'undefined') str='';
 	if (typeof str !== 'string') str=str+'';
 	let ret = {left:''};
 	
 	for (let i=0; i < str.length; i++) {
-		let op = _is_operator(str, i);
+		let op = _is_operator(str, i, opt);
 		if (op) {
 			ret.delimiter = op;
 			ret.right = str.substring(i + 1);
@@ -20,27 +18,36 @@ const parse_filter = (str) => {
 	}
 	if (!ret.right) ret.left = str;
 
-	ret.left = _replace_escaped_operators(ret.left);
-	if (ret.right) ret.right = _replace_escaped_operators(ret.right);
+	ret.left = _replace_escaped_operators(ret.left, opt);
+	if (ret.right) ret.right = _replace_escaped_operators(ret.right, opt);
 
 	return ret;
 }
 
-const _replace_escaped_operators = (str) => {
+const _replace_escaped_operators = (str, opt) => {
 	let ret = str.trim();
+	const operator_keys = _get_operator_keys(opt);
 	operator_keys.forEach(_itm => {
 		ret = ret.replace("\\"+_itm, _itm);
 	})
 	return ret;
 }
 
-const _is_operator = (str, str_index) => {
+const _is_operator = (str, str_index, opt) => {
 	let ret;
+	const operator_keys = _get_operator_keys(opt);
 	for (let i=0; i<operator_keys.length && !ret; i++){
 		let op = _is_operator_word(str, str_index, operator_keys[i]);
 		if (op) ret = operator_keys[i];
 	}
 	return ret;
+}
+
+const _get_operator_keys = (opt) => {
+	const localOperator = Object.assign({}, operator, (opt || {}).operator);
+	const operator_keys = Object.keys(localOperator);
+	operator_keys.sort((a,b)=>{return b.length - a.length}); // if "=" comes before "*=" then "=" will win while it is really "*=", not "="
+	return operator_keys;
 }
 
 const _is_operator_word = (str, str_index, word) => {
