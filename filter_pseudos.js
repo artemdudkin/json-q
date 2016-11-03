85527b21bfa773319bc5350c703d21c54143b334
artemdudkin <sheeva@list.ru>
2016-11-03 10:59:23 +0300

    refactoring

--- filter_pseudos.js
+++ filter_pseudos.js
@@ -1,11 +1,14 @@
 //
 //remember that 
-//	value is always not-array (may be object)
+//	1. arrValue is always array 
+//	2. pseudo should return array
 //
 const pseudo = {
 
-	"empty" : function(value){
-		if (typeof value !== 'object') return value;
+	"empty" : function(arrValue){
+		return arrValue.filter(_itm => {
+			return (typeof _itm !== 'object')
+		})
 	}
 
 }
