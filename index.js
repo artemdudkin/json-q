672c5587a00afee174c1f49819638658d6a2eafe
unknown <sheeva@list.ru>
2016-11-08 08:46:33 +0300

    more TODOs

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 //FIX fix difference between wsdl parsed by json-q and by old parser
 
-//TODO make it works with browsers (IE9+) => json-q.min.js, size?
+//TODO make it works with browsers (IE9+) => json-q.min.js, size? 
+//TODO AMD support?
 //TODO add strings? (and remove _replace_escaped_operators)
 //TODO performance?
 
