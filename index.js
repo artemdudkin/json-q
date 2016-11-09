c2a89b632329863e917b283b0219475fa788e708
artemdudkin <sheeva@list.ru>
2016-11-09 12:45:40 +0300

    update TOSOs

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 //FIX fix difference between wsdl parsed by json-q and by old parser
 
-//TODO AMD support?
-//TODO add strings? (and remove _replace_escaped_operators)
-//TODO performance?
+//TODO? get rid of clone (https://github.com/pvorb/node-clone) as it uses Buffers which means adding 23k minified Buffers lib at browser?
+//TODO? AMD support?
+//TODO? add strings? (and remove _replace_escaped_operators)
+//TODO? performance?
 
 const { get } = require('./lib/index');
 
