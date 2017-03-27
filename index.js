f4b7c305f935dd154bfa11e48a29caf400254206
Artem Dudkin <sheeva@list.ru>
2017-03-27 18:42:38 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 //TODO cannot add filter after pseudo (while can add pseudo after filter)
 //TODO get({ "a[c]":1 }, "a[c]")) returns [] silently -> should return 'illegal filter'
+//TODO "a[c" does not produce error 'filter not closed'
 
 //TODO? AMD support?
 //TODO? add strings? (and remove _replace_escaped_operators)
